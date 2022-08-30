f66befb5e858e403227f3d676d88cc186da58975
streetms <konstantin.isakov.2003@mail.ru>
2022-08-30 19:16:00 +0700

    add pointer of funtion in Lexem

--- Lexeme.h
+++ Lexeme.h
@@ -1,10 +1,12 @@
 #ifndef LEXEM
 #define LEXEM
 #include <string>
+#include <functional>
 
 class Lexeme
 {
 public:
+    std::function<double(double)> f;
     enum class Type {function, number, bracket, operator_, variable, undefined};
     Type type;
     int priority;
